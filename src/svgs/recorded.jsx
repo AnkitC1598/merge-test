526146f5f4d422dc0771f4f3d6a21b7faa856cb3
AnkitC1598 <ankitc@letsupgrade.in>
2023-07-31 15:36:53 +0530

    chore: minor jsx changes for svg

--- src/svgs/recorded.jsx
+++ src/svgs/recorded.jsx
@@ -10,16 +10,16 @@ const RecordedIcon = ({ className }) => {
 				<path
 					d="M62.6957 47.4638L49.1075 38.405C46.2831 36.5221 42.5 38.5467 42.5 41.9412V60.0588C42.5 63.4533 46.2831 65.4779 49.1075 63.595L62.6957 54.5362C65.219 52.854 65.219 49.146 62.6957 47.4638Z"
 					stroke="currentColor"
-					stroke-width="6"
-					stroke-linecap="round"
-					stroke-linejoin="round"
+					strokeWidth="6"
+					strokeLinecap="round"
+					strokeLinejoin="round"
 				/>
 				<path
 					d="M89.25 51C89.25 72.1249 72.1249 89.25 51 89.25C29.8751 89.25 12.75 72.1249 12.75 51C12.75 29.8751 29.8751 12.75 51 12.75C72.1249 12.75 89.25 29.8751 89.25 51Z"
 					stroke="currentColor"
-					stroke-width="6"
-					stroke-linecap="round"
-					stroke-linejoin="round"
+					strokeWidth="6"
+					strokeLinecap="round"
+					strokeLinejoin="round"
 				/>
 			</svg>
 		</>
--- src/svgs/rte.jsx
+++ src/svgs/rte.jsx
@@ -1,5 +1,3 @@
-import React from "react"
-
 const RTEIcon = ({ className }) => {
 	return (
 		<>
@@ -12,9 +10,9 @@ const RTEIcon = ({ className }) => {
 				<path
 					d="M46.75 46.75V38.25C46.75 33.5556 42.9444 29.75 38.25 29.75M46.75 46.75V63.75C46.75 68.4444 50.5556 72.25 55.25 72.25C59.9444 72.25 63.75 68.4444 63.75 63.75V59.5M46.75 46.75H38.25M46.75 46.75H63.75M89.25 51C89.25 72.1249 72.1249 89.25 51 89.25C29.8751 89.25 12.75 72.1249 12.75 51C12.75 29.8751 29.8751 12.75 51 12.75C72.1249 12.75 89.25 29.8751 89.25 51Z"
 					stroke="currentColor"
-					stroke-width="8"
-					stroke-linecap="round"
-					stroke-linejoin="round"
+					strokeWidth="8"
+					strokeLinecap="round"
+					strokeLinejoin="round"
 				/>
 			</svg>
 		</>
